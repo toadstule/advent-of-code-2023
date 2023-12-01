e066b9c08e7cda106b0d6d5963251398e96faa42
Steve Jibson <steve@jibson.com>
2023-11-30 23:22:54 -0700

    Day 01

--- aoc/d01.py
+++ aoc/d01.py
@@ -1,7 +1,7 @@
-"""Advent of Code 2022 -- Day 1: DAY_TITLE.
+"""Advent of Code 2022 -- Day 1: Trebuchet?!.
 
 Reference: https://adventofcode.com/2022/day/1
-"""  # Trebuchet?!
+"""
 from jibaoc import solver
 
 
