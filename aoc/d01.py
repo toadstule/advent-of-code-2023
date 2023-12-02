6ea1f7a90cafac63b0ed8f32dcab52c2418fbf0d
Steve Jibson <steve@jibson.com>
2023-12-01 18:30:23 -0700

    Day 01 - revisited

--- aoc/d01.py
+++ aoc/d01.py
@@ -2,11 +2,28 @@
 
 Reference: https://adventofcode.com/2023/day/1
 """
+from typing import Final, Optional
+
 from jibaoc import solver
 
 
 class D01(solver.Solver):
     """Solve puzzle for day 1."""
+
+    _TEXT_DIGITS: Final[dict[int, str]] = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+    }
+    _REVERSE_TEXT_DIGITS: Final[dict[int, str]] = {
+        k: "".join(reversed(v)) for k, v in _TEXT_DIGITS.items()
+    }
 
     def __post_init__(self) -> None:
         """Initialize a D01 instance."""
@@ -14,48 +31,41 @@ class D01(solver.Solver):
 
     def part_one(self) -> int:
         """Return the sum of all the calibration values."""
-        result: int = 0
-        for line in self._calibration_doc:
-            digits: list[int] = [int(char) for char in line if char.isdigit()]
-            result += digits[0] * 10 + digits[-1]
-        return result
+        return self._get_calibration_value(include_text_digits=False)
 
     def part_two(self) -> int:
         """Return the sum of all the calibration values; include text-represented digits."""
-        text_digits: dict[int, str] = {
-            1: "one",
-            2: "two",
-            3: "three",
-            4: "four",
-            5: "five",
-            6: "six",
-            7: "seven",
-            8: "eight",
-            9: "nine",
-        }
-        result: int = 0
-        for line in self._calibration_doc:
-            first_digit: int = 0
-            last_digit: int = 0
-            for i, char in enumerate(line):
-                if char.isdigit():
-                    first_digit = int(char)
-                    break
+        return self._get_calibration_value(include_text_digits=True)
+
+    def _get_calibration_value(self, include_text_digits: bool) -> int:
+        """Return the calibration value from the calibration doc."""
+        return sum(
+            self._get_first_digit(line, include_text_digits) * 10
+            + self._get_last_digit(line, include_text_digits)
+            for line in self._calibration_doc
+        )
+
+    def _get_first_digit(self, line: str, include_text_digits: bool = False) -> int:
+        """Return the first digit in the given string, times 10."""
+        return self._get_digit(
+            line=line, text_digits=D01._TEXT_DIGITS if include_text_digits else None
+        )
+
+    def _get_last_digit(self, line: str, include_text_digits: bool = False) -> int:
+        """Return the last digit in the given string."""
+        return self._get_digit(
+            line="".join(reversed(line)),
+            text_digits=D01._REVERSE_TEXT_DIGITS if include_text_digits else None,
+        )
+
+    @staticmethod
+    def _get_digit(line: str, text_digits: Optional[dict[int, str]]) -> int:
+        """Return the first digit in the given string, optionally using given text_digits."""
+        for i, char in enumerate(line):
+            if char.isdigit():
+                return int(char)
+            if text_digits:
                 for digit, text in text_digits.items():
                     if line[i:].startswith(text):
-                        first_digit = digit
-                        break
-                if first_digit:
-                    break
-            for i, char in reversed(list(enumerate(line))):
-                if char.isdigit():
-                    last_digit = int(char)
-                    break
-                for digit, text in text_digits.items():
-                    if line[i - 1 :].startswith(text):
-                        last_digit = digit
-                        break
-                if last_digit:
-                    break
-            result += first_digit * 10 + last_digit
-        return result
+                        return digit
+        raise ValueError("Unable to find digit in line")
